42ab8c175956f05af4bca037358a20297b771be2
srini88 <pvsrinivas.ece@gmail.com>
2016-05-24 23:38:22 -0500

    fill array easy way

--- js.js
+++ js.js
@@ -1,7 +1,2 @@
-var char_set = Array.apply(null, Array(256)).map(function(item, index){
-	return false;
-}); 
-
-console.log(char_set) //you get the same result......
-
-// http://stackoverflow.com/questions/37427133/fill-in-the-values-of-an-array-using-the-array-map?noredirect=1#comment62360238_37427133
+var arr = new Array(256).fill(false);
+console.log(arr); //easiet way to fill an array..
