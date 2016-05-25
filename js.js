057ee039dcd79a5abbdc6fb30466673d2bec4c52
srini88 <pvsrinivas.ece@gmail.com>
2016-05-24 22:34:37 -0500

    2

--- js.js
+++ js.js
@@ -1,31 +1,15 @@
-// Implement an algorithm to determine if a string has all unique characters. What if
-// you cannot use additional data structures?
+// Implement a function void reversefchar* str) in Cor C++ which reverses a null-terminated
+// string.
+
+var reverseString = function(str) {
+      var len = str.length;
+      console.log(str, len)
+      if (len > 1) {
+      	
+        return str[len - 1] + reverseString(str.substring(1, len - 1)) + str[0];
+      }
+      return str; ///this is the base case.....at that time no string at all, len will be zero,, this wil be returned to the return in the above if...and that stuff will be plussed..
+    }
 
 
-// ASCII has a total of 128 characters (256 in the extended set).
-
-// ASCII defines 128 characters, which map to the numbers 0–127. Unicode defines (less than) 221 characters, which, similarly, map to numbers 0–221 (though not all numbers are currently assigned, and some are reserved).
-
-// Unicode is a superset of ASCII, and the numbers 0–128 have the same meaning in ASCII as they have in Unicode. For example, the number 65 means "Latin capital 'A'".
-
-
-// http://stackoverflow.com/questions/37427133/fill-in-the-values-of-an-array-using-the-array-map
-
-var isUnique = function(str) {
-    //can't have unique characters if string length is greater than the # of possible characters
-	if (str.length > 256) return false; 
-	for (var i = 0; i< str.length; i++) {
-		//console.log(str.indexOf(str[i]))
-		//console.log(str.lastIndexOf(str[i])===i)
-		if (str.indexOf(str[i]) !== str.lastIndexOf(str[i]) ) {
-
-			return false;
-		}
-	}
-	return true;
-   }
-
-console.log(isUnique("abcdevfa")) //false
-
-// 'canal'.lastIndexOf('a');     // returns 3
-// 'canal'.lastIndexOf('x');     // returns -1
+console.log(reverseString("sriniv"))
