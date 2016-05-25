54c896dbe1c5c66466a824aad7760dd6a6043bef
srini88 <pvsrinivas.ece@gmail.com>
2016-05-24 23:54:20 -0500

    boo

--- js.js
+++ js.js
@@ -1,24 +1,31 @@
-// Write a method to replace all spaces in a string with '%20'. You may assume that the
-// string has sufficient space at the end of the string to hold the additional characters,
-// and that you are given the "true" length of the string. (Note: if implementing in Java,
-// please use a character array so that you can perform this operation in place.)
+// Implement a method to perform basic string compression using the counts of
+// repeated characters. For example, the string aabcccccaaa would become
+// a2blc5a3. If the "compressed" string would not become smaller than the original
+// string, your method should return the original string.
 
 
+var compression = function(str){
 
+	var output ="";
+	var count =1;
+	var temp = str[0];
 
-// This could be done fairly simply by using the String.replace() function. 
-// I elected to not do that for the sake of making this problem a little less trivial
-// */
-
-function replaceWhiteSpace(str) {
-	var outputArr = [];
-	for (var i=0; i< str.length; i++) {
-		if (str[i] === ' ') {
-			outputArr.push('%20');
+	for (var i=1; i < str.length ;++i){
+		if (str[i] === temp){
+			++count;
 		}
-		else outputArr.push(str[i]);
- 	}
- 	return outputArr.join('');
+		else{
+			output += temp+count;  /// having a[i] +count giving wrong
+			temp = str[i];
+			count =1;
+		}
+
+	}
+	output += temp+count;
+	return output;
+
+
+
 }
 
-console.log(replaceWhiteSpace("Hello World Srini"))  //Hello%20World%20Srini
+console.log(compression("aabcccccaaa"));
