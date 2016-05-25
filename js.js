f96a253f8af030ec92a17e2ba437b28116e3f863
srini88 <pvsrinivas.ece@gmail.com>
2016-05-24 23:11:07 -0500

    boo

--- js.js
+++ js.js
@@ -1,15 +1,31 @@
-// Implement a function void reversefchar* str) in Cor C++ which reverses a null-terminated
-// string.
+// Given two strings, write a method to decide if one is a permutation of the other.
 
-var reverseString = function(str) {
-      var len = str.length;
-      console.log(str, len)
-      if (len > 1) {
-      	
-        return str[len - 1] + reverseString(str.substring(1, len - 1)) + str[0];
-      }
-      return str; ///this is the base case.....at that time no string at all, len will be zero,, this wil be returned to the return in the above if...and that stuff will be plussed..
-    }
+// If two strings are anagrams, then we know they have the same characters, but in different
+// orders. Therefore, sorting the strings will put the characters from two anagrams in the
+// same order. We just need to compare the sorted versions of the strings
+
+var anagram  = function(str1, str2){
+	if (str1.length !== str2.length)
+		return false;
+
+	str1Arr = str1.split("").sort(); ///has to be assigned..split is not mutable..
+	console.log(str1Arr); //you got elements array
+	str2Arr = str2.split("").sort(); //["i", "i", "n", "r", "s"]
+	console.log(str2Arr);
+
+	//var anagram = true;
+	str1Arr.forEach( function(element, index) {
+		if (str2Arr[index] !== element)
+		{
+			console.log(str2Arr[index])
+			return false;  ///this returns from the callback function...not from our original anagram fun//wrong sol
+		}
+	});
+	return true;
 
 
-console.log(reverseString("sriniv"))
+
+}
+
+console.log(anagram("ksrini", "inirss"));
+
