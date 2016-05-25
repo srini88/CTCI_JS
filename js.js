374f8a250ea0440be97f2b07628c3f11938c4ea2
srini88 <pvsrinivas.ece@gmail.com>
2016-05-24 23:36:27 -0500

    fill an array

--- js.js
+++ js.js
@@ -1,29 +1,7 @@
-// // Given two strings, write a method to decide if one is a permutation of the other.
-// We can also use the definition of an anagram—two words with the same character
-// counts—to implement this algorithm. We simply iterate through this code, counting
-// how many times each character appears.Then, afterwards, we compare the two arrays
+var char_set = Array.apply(null, Array(256)).map(function(item, index){
+	return false;
+}); 
 
-var anagram  = function(str1, str2){
-	
-	//do with one map
-	if (str1.length !== str2.length) 
-		return false;
-	var map =[];
-	for (var i=0 ; i < str1.length ; ++i){
-		map[str1[i]] = (typeof map[str1[i]] === 'undefined')? 1: ++map[str1[i]];
-	}	
+console.log(char_set) //you get the same result......
 
-	for (var j=0; j <str2.length;++j){
-		if (map[str2[j]] >0){
-			--map[str2[j]];
-		}
-		else
-			return false;
-	}
-	//console.log(map)
-	return true;
-
-}
-
-console.log(anagram("srinffi", "iffnir"));
-
+// http://stackoverflow.com/questions/37427133/fill-in-the-values-of-an-array-using-the-array-map?noredirect=1#comment62360238_37427133
