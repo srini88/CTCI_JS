04d09831103bf8d3b28e18e095dd62443a970208
srini88 <pvsrinivas.ece@gmail.com>
2016-05-24 22:20:38 -0500

    1.1

--- js.js
+++ js.js
@@ -12,17 +12,20 @@
 // http://stackoverflow.com/questions/37427133/fill-in-the-values-of-an-array-using-the-array-map
 
 var isUnique = function(str) {
-      var char_set = Array.apply(null, Array(256)).map(Boolean.prototype.valueOf, false); //fills it with false, if you put true fills it with true..
-      console.log(char_set); //all false 256  /// index 0-255  , value false..
-      console.log(char_set.length) //256
-      for (var i = 0; i < str.length; i++) {
-      	//console.log(char_set[str[i].charCodeAt(0)], str[i].charCodeAt(0), str[i]) //you got only one letter..you calculating character code at 0, since one letter is there..
-        if (char_set[str[i].charCodeAt(0)]) {
-          return false;
-        }
-        char_set[str[i].charCodeAt(0)] = true;
-      }
-      return true;
+    //can't have unique characters if string length is greater than the # of possible characters
+	if (str.length > 256) return false; 
+	for (var i = 0; i< str.length; i++) {
+		//console.log(str.indexOf(str[i]))
+		//console.log(str.lastIndexOf(str[i])===i)
+		if (str.indexOf(str[i]) !== str.lastIndexOf(str[i]) ) {
+
+			return false;
+		}
+	}
+	return true;
    }
 
-console.log(isUnique("abcdevfa"))
+console.log(isUnique("abcdevfa")) //false
+
+// 'canal'.lastIndexOf('a');     // returns 3
+// 'canal'.lastIndexOf('x');     // returns -1
