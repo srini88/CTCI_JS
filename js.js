e8254275a8ecb589569c4740a1993cff4522c99d
srini88 <pvsrinivas.ece@gmail.com>
2016-05-24 23:22:18 -0500

    initial

--- js.js
+++ js.js
@@ -1,29 +1,19 @@
-// Given two strings, write a method to decide if one is a permutation of the other.
-
-// If two strings are anagrams, then we know they have the same characters, but in different
-// orders. Therefore, sorting the strings will put the characters from two anagrams in the
-// same order. We just need to compare the sorted versions of the strings
+// // Given two strings, write a method to decide if one is a permutation of the other.
+// We can also use the definition of an anagram—two words with the same character
+// counts—to implement this algorithm. We simply iterate through this code, counting
+// how many times each character appears.Then, afterwards, we compare the two arrays
 
 var anagram  = function(str1, str2){
-	if (str1.length !== str2.length)
-		return false;
+	
+	//do with one map
+	var map =[];
+	for (var i=0 ; i < str1.length ; ++i){
+		map[str1[i]] = (typeof map[str1[i]] === 'undefined')? 1: ++map[str1[i]];
 
-	str1Arr = str1.split("").sort(); ///has to be assigned..split is not mutable..
-	console.log(str1Arr); //you got elements array
-	str2Arr = str2.split("").sort(); //["i", "i", "n", "r", "s"]
-	console.log(str2Arr);
+		   //have to use quotations for undefined..
 
-	var anagram = true;
-	str1Arr.forEach( function(element, index) {
-		if (str2Arr[index] !== element)
-		{
-			anagram = false;
-			return 5;  ///this returns from the callback function...whatever return you give does not matter..
-			/// best to put return;
-		}
-	});
-	return anagram;
-
+	}	
+	console.log(map) //[s: 1, r: 1, i: 2, n: 1]
 
 
 }
