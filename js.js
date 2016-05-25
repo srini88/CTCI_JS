8c0caa2c3155b6fa927c4b88488abb3b14a5a014
srini88 <pvsrinivas.ece@gmail.com>
2016-05-25 16:00:32 -0500

    add element to array and shift

--- js.js
+++ js.js
@@ -1,12 +1,19 @@
-///remove duplicates in a string
+//add an element to an array and mnove the nesxt indices...
 
-var str1 = "Helloooo";
+var arr = [0, 1, 2, 4, 5, 6, 7];
 
-var result ="";
-for (var i=0 ; i < str1.length; ++i){
-	console.log(result.indexOf(str1[i]))
-	if (result.indexOf(str1[i]) === -1){  //leka pothe add cheyi
-		result +=str1[i];
-	}
+var newElement = 3;
+var index = 3;
+
+//go to the index directly..
+for (var i = arr.length-1; i >=index ; --i){
+	arr[i+1] = arr[i];
 }
-console.log(result) //Helo
+console.log(arr); //[0, 1, 2, 4, 4, 5, 6, 7]
+arr[index] = newElement;
+
+console.log(arr);
+
+
+
+
