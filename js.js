eab41fdcafef0e5bde166294f37f92f8e4a645de
srini88 <pvsrinivas.ece@gmail.com>
2016-05-28 18:03:36 -0500

    parantheses

--- js.js
+++ js.js
@@ -1,61 +1,29 @@
 
+function generateparen(left,right,str)
+{
+	var arr = [];
+    if(right==0)
+    {
+        console.log(str);
+        return;
+    }
+    if(left>0)
+    {
+    	console.log("1st left: "+left, "right: "+ right, "str: " +str);
+    	generateparen(left-1,right,str+'(');
+    }
 
-//division works for all cases...
-
-function division(a, b){
-
-	var absa = abs(a);
-	var absb = abs(b);
-
-	if (absa < absb ) return 0;
-	if (absb ===0) return "cannot divide by zero";
-	if (a === 0) return 0;  //if a is zero
-	//we get into 
-
-	var result = 0;
-	var count = 0;
-	while(result <= absa){
-
-		result += absb;
-		++count;
-	}
-	count = --count;
-	if ((a <0 &&  b < 0) || (a>0 && b>0))
-		return count;
-	else
-		return negate(count);
-
+    	
+    if(right>left)
+    {
+    	console.log("2nd left: "+left, " right: "+ right, "str: "+str);
+    	generateparen(left,right-1,str+')');
+    }
+ 
 }
 
-console.log(division(25, 0.8))
+    var n=3; 
+    var str="";
+    generateparen(n,n,str);
 
 
-
-//absolute function to always return +ve if x is +ve or -ve
-function abs (x){
-
-	if (x > 0) return x;
-
-	return negate(x);
-
-}
-//does negation of a number
-function negate(x){
-	var i = (x >0) ? -1 : 1;
-	var a = 0;
-
-	while (x!==0){
-
-		a += i;
-		x += i;
-	}
-
-	console.log("a: " +a);
-	console.log("x: " + x);
-	return a;  ///
-
-}
-
-//console.log(negate(5))
-
-//console.log(division(15, 3));
