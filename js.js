46a53f5336814f20f293bd5264ec9fba8843c7e6
srini88 <pvsrinivas.ece@gmail.com>
2016-05-28 18:06:09 -0500

    parantheses in an array

--- js.js
+++ js.js
@@ -1,29 +1,31 @@
 
-function generateparen(left,right,str)
+function generateparen(left,right,str, arr)
 {
-	var arr = [];
+	
     if(right==0)
     {
-        console.log(str);
+        arr.push(str);
         return;
     }
     if(left>0)
     {
-    	console.log("1st left: "+left, "right: "+ right, "str: " +str);
-    	generateparen(left-1,right,str+'(');
+    	//console.log("1st left: "+left, "right: "+ right, "str: " +str);
+    	generateparen(left-1,right,str+'(', arr);
     }
 
     	
     if(right>left)
     {
-    	console.log("2nd left: "+left, " right: "+ right, "str: "+str);
-    	generateparen(left,right-1,str+')');
+    	//console.log("2nd left: "+left, " right: "+ right, "str: "+str);
+    	generateparen(left,right-1,str+')',arr);
     }
  
+ 	return arr;
 }
 
     var n=3; 
     var str="";
-    generateparen(n,n,str);
+    var arr=[];
+    console.log(generateparen(n,n,str, arr));
 
 
