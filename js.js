b1adc30e8ae7347a1bec559abd004dcebd300543
srini88 <pvsrinivas.ece@gmail.com>
2016-05-29 02:16:08 -0500

    more explanation

--- js.js
+++ js.js
@@ -11,13 +11,5 @@ for (var i = 0; i < nodes.length; i++) {
         }
     })(i));  
 }
+// The reason is that after the for loop is completed, the variable i assumes a value equal to the length of the nodes list. In addition, because i was in scope at the time the code attached the handler, the variable belongs to handler’s closure. As you’ll recall, the value of the variables in closures isn’t static, hence the value of i isn’t the value at the time the handler was added (0 for the first button in the list, 1 for the second, and so on). At the time the handler will be executed, on the console will be printed the current value of the variable i, that is equal to the length of the nodes list.
 
-///working nowwwwwwwwwwwwww...........also see http://stackoverflow.com/questions/37495673/javascript-onclick-event-not-firing-correctly
-
-// Here is what it is happening in you code:
-
-// ==> for loop gets executed synchronously as it is part of javascript engine post which javascript handles event queue which is a FIFO (first in first out)
-
-// ==> When for loop finished value of i is three which remains in memory until the function inside it executes
-
-// ==> Each time it takes a value 3 and prints it.
