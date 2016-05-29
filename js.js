ea9dc9d4bac4c50187bad8db928434650ca248c4
srini88 <pvsrinivas.ece@gmail.com>
2016-05-29 02:09:51 -0500

    partial

--- js.js
+++ js.js
@@ -3,9 +3,10 @@ var nodes = document.
    
 
 for (var i = 0; i < nodes.length; i++) {
-   nodes[i].addEventListener('click', function() {
-     console.log('You clicked element #' + i);
-   });  
+    ////converted click function into an IIFE so it executes then and there only 
+    nodes[i].addEventListener('click', (function(j) {
+        console.log('You clicked element #' + j);
+    })(i));  
 }
 
-//this code by default prints you clicked element 3....problem......fix it 
+////to some extent this stuff works..yuo could see IIFE running immediely...console shows 0 1 2....but you want to show only when we click...so we got to reutn the stuff instead of just consoling..
