f4115c8cec2da3ca320ec0f266a4f896e16db81e
srini88 <pvsrinivas.ece@gmail.com>
2016-05-29 02:24:30 -0500

    2nd solutuno

--- js.js
+++ js.js
@@ -4,13 +4,12 @@ var nodes = document.
 
 for (var i = 0; i < nodes.length; i++) {
 
-    nodes[i].addEventListener('click', function() {
-
-        
-            console.log('You clicked element #' + i);
-        
-    });  
+    nodes[i].addEventListener('click', handleWrapper(i) );  
 }
-// 
+function handleWrapper(i){
+    return function(){
+       console.log('You clicked element #' + i); 
+    }
+}
 
-// back to original problem
+// awesome solution above....
