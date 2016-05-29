bda4554191a75ec43726cb6e94afcd2e1cd2dc8d
srini88 <pvsrinivas.ece@gmail.com>
2016-05-28 23:46:48 -0500

    z value retained

--- js.js
+++ js.js
@@ -3,14 +3,16 @@ function outerFunction(x){
     var z=3;
 
     return function(y){
-        console.log(x+y+z);
+        console.log("z: " +z);
+        console.log(x+y+(++z));
     }
 }
 
-var innerFunction = outerFunction(2);  //inner function is now a closure..
-//by calling  outerFunction it sets the values x=2 and z=3 and returns a function which takes a y and gives us the sum ..
+var innerFunction = outerFunction(2);  
 
-//the returning function get assigned to innerFunction() on return..in geenral variables x and z should be destroyed as outerFunction() has completed its execution and it returned successfully...
-///but closures say it exists for innerFunction and whenever we call innerFunction values of x and y will always be same defined by the outerfunction..
+innerFunction(1); //7
+innerFunction(1);  //8
+innerFunction(1);  //9
+innerFunction(1);  //10
 
-console.log(innerFunction); //you get a function back
+// z value is tracked...we doing ++z in the code...when we call innerFunction again z value is persisted and state is retained..
