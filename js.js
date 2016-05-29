add6de9a36d8cbb3ca2d9e782ce8e33f585d4901
srini88 <pvsrinivas.ece@gmail.com>
2016-05-29 00:09:38 -0500

    closure modifying the default object

--- js.js
+++ js.js
@@ -3,31 +3,20 @@ function outerFunction(x){
     var z=3;
 
     return function(y){
-        x=x+1;
+        console.log(x);
+        ++x.a;
         console.log(x+y+z);
     }
 }
 
-var myVal = new Number(2);
-console.log(myVal);
+var myObj = { a:2} //myVal is an object
+//console.log(myVal); //Number {[[PrimitiveValue]]: 2}
+///myVal is passed as x which is a primitive obj
 
-// The Number() function converts the object argument to a number that represents the object's value.
+var innerFunction  = outerFunction(myObj); //you are passing in object right...
+innerFunction(10)   //im passing y as 10
+innerFunction(10)
+innerFunction(10)
 
-// If the value cannot be converted to a legal number, NaN is returned.  
+console.log(myObj);  //this myObj has updated value...because closure updates the value..
 
-Boolean(expression) will simply convert the expression into a boolean primitive value, while new Boolean(expression) will create a wrapper object around the converted boolean value.
-
-// Note I'm using strict-equals
-new Boolean("true") === true; // false
-Boolean("true") === true; // true
-
-typeof new Boolean("true"); // "object"
-typeof Boolean("true"); // "boolean"
-
-new Number( x )
-creates a new wrapper object. I don't think that there is a valid reason to ever use this.
-
-Number( x )
-converts the passed argument into a Number value. You can use this to cast some variable to the Number type. However this gets the same job done:
-
-The Number JavaScript object is a wrapper object allowing you to work with numerical values. A Number object is created using the Number() constructor.
