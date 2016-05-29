2a994863bfc51d613c914d74546ea9696a2652ec
srini88 <pvsrinivas.ece@gmail.com>
2016-05-29 02:12:16 -0500

    IIFE correct solution

--- js.js
+++ js.js
@@ -3,10 +3,21 @@ var nodes = document.
    
 
 for (var i = 0; i < nodes.length; i++) {
-    ////converted click function into an IIFE so it executes then and there only 
+
     nodes[i].addEventListener('click', (function(j) {
-        console.log('You clicked element #' + j);
+
+        return function(){
+            console.log('You clicked element #' + j);
+        }
     })(i));  
 }
 
-////to some extent this stuff works..yuo could see IIFE running immediely...console shows 0 1 2....but you want to show only when we click...so we got to reutn the stuff instead of just consoling..
+///working nowwwwwwwwwwwwww...........also see http://stackoverflow.com/questions/37495673/javascript-onclick-event-not-firing-correctly
+
+// Here is what it is happening in you code:
+
+// ==> for loop gets executed synchronously as it is part of javascript engine post which javascript handles event queue which is a FIFO (first in first out)
+
+// ==> When for loop finished value of i is three which remains in memory until the function inside it executes
+
+// ==> Each time it takes a value 3 and prints it.
