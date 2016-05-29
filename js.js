ceab4225f0c679863bae64a1fb02a2ad6cb94931
srini88 <pvsrinivas.ece@gmail.com>
2016-05-29 02:02:44 -0500

    correct solution for closure

--- js.js
+++ js.js
@@ -6,18 +6,17 @@ var myFunctions ={};
 
 for (var i=0; i<3 ; i++){ //putting functions inside myFunctions..
 
-    //in the previous commit...you are setting the function..bbut you are not running the function...very very imp....
-    //////the function wont run until you call them..we want it to run and save the context of i...
     myFunctions[i] = (function(i){
-        console.log("my value: "+i);
+        return function(){  //now returning a function which will be put into each index...and since IIFE it will be executed immediately..
+            console.log("my value: "+i);
+        }
+        
+    
     })(i);
 
 }
 
-// for (var j=0; j<3 ; j++){
-//     myFunctions[j]();  //that's why parantheses to run them
-// }
-console.log(myFunctions)
-//prints Object {0: undefined, 1: undefined, 2: undefined}
+for (var j=0; j<3 ; j++){
+    myFunctions[j]();  //that's why parantheses to run them
+}
 
-//because the function has not returned.. //worng syntax....you got to return something...and that return has to be a function..because you are storing function inside myFunctions[i]
