857478ba8c94d4ef4bccd26d062e550331ee64a0
srini88 <pvsrinivas.ece@gmail.com>
2016-05-28 23:56:17 -0500

    number wrapper notes

--- js.js
+++ js.js
@@ -3,16 +3,31 @@ function outerFunction(x){
     var z=3;
 
     return function(y){
-        console.log("z: " +z);
-        console.log(x+y+(++z));
+        x=x+1;
+        console.log(x+y+z);
     }
 }
 
-var innerFunction = outerFunction(2);  
+var myVal = new Number(2);
+console.log(myVal);
 
-innerFunction(1); //7
-innerFunction(1);  //8
-innerFunction(1);  //9
-innerFunction(1);  //10
+// The Number() function converts the object argument to a number that represents the object's value.
 
-// z value is tracked...we doing ++z in the code...when we call innerFunction again z value is persisted and state is retained..
+// If the value cannot be converted to a legal number, NaN is returned.  
+
+Boolean(expression) will simply convert the expression into a boolean primitive value, while new Boolean(expression) will create a wrapper object around the converted boolean value.
+
+// Note I'm using strict-equals
+new Boolean("true") === true; // false
+Boolean("true") === true; // true
+
+typeof new Boolean("true"); // "object"
+typeof Boolean("true"); // "boolean"
+
+new Number( x )
+creates a new wrapper object. I don't think that there is a valid reason to ever use this.
+
+Number( x )
+converts the passed argument into a Number value. You can use this to cast some variable to the Number type. However this gets the same job done:
+
+The Number JavaScript object is a wrapper object allowing you to work with numerical values. A Number object is created using the Number() constructor.
